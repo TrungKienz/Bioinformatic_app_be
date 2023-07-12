b87cbb0383b341046187fb29751dab79edffc177
klight <phamminhquang2k2@gmail.com>
2023-07-13 01:52:47 +0700

    up

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
-const URL = process.env.DATABASE;
+const URL =
+    'mongodb+srv://root:AVkqPYy7QKS26JuT@bioinformatic.ulibahi.mongodb.net/?retryWrites=true&w=majority';
+// const URL = 'mongodb://localhost:27017/Informatic_Lab';
 
 const connect = async () => {
     try {
         await mongoose.connect(URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            // useCreateIndex: false,
+            // useFindAndModify: false,
         });
         console.log('Connected to mongoDB');
     } catch (error) {
